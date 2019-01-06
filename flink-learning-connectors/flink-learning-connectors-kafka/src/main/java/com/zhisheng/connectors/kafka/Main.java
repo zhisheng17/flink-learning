2e4b0179d2b03834cbdccba05c31fab3404eb0bb
田志声 <zhisheng.tzs@alibaba-inc.com>
2019-01-06 22:20:48 +0800

    add flink connectors kafka

--- flink-learning-connectors/flink-learning-connectors-kafka/src/main/java/com/zhisheng/connectors/kafka/Main.java
+++ flink-learning-connectors/flink-learning-connectors-kafka/src/main/java/com/zhisheng/connectors/kafka/Main.java
@@ -2,18 +2,26 @@ package com.zhisheng.connectors.kafka;
 
 
 import com.zhisheng.common.model.Metrics;
+import com.zhisheng.common.schemas.MetricSchema;
 import com.zhisheng.common.utils.ExecutionEnvUtil;
 import com.zhisheng.common.utils.KafkaConfigUtil;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
 
 public class Main {
     public static void main(String[] args) throws Exception{
         final ParameterTool parameterTool = ExecutionEnvUtil.createParameterTool(args);
         StreamExecutionEnvironment env = ExecutionEnvUtil.prepare(parameterTool);
         DataStreamSource<Metrics> data = KafkaConfigUtil.buildSource(env);
-        data.print();
+
+        data.addSink(new FlinkKafkaProducer011<Metrics>(
+                parameterTool.get("kafka.sink.brokers"),
+                parameterTool.get("kafka.sink.topic"),
+                new MetricSchema()
+                )).name("flink-connectors-kafka")
+                .setParallelism(parameterTool.getInt("stream.sink.parallelism"));
 
         env.execute("flink learning connectors kafka");
     }
