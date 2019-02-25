7548fc68eb06b6be5fa8c379ca9de0901d8038e4
田志声 <zhisheng.tzs@alibaba-inc.com>
2019-02-26 00:29:44 +0800

    add constant

--- flink-learning-common/src/main/java/com/zhisheng/common/constant/PropertiesConstants.java
+++ flink-learning-common/src/main/java/com/zhisheng/common/constant/PropertiesConstants.java
@@ -4,12 +4,15 @@ package com.zhisheng.common.constant;
 public class PropertiesConstants {
     public static final String ZHISHENG = "zhisheng";
     public static final String KAFKA_BROKERS = "kafka.brokers";
+    public static final String DEFAULT_KAFKA_BROKERS = "localhost:9092";
     public static final String KAFKA_ZOOKEEPER_CONNECT = "kafka.zookeeper.connect";
+    public static final String DEFAULT_KAFKA_ZOOKEEPER_CONNECT = "localhost:2181";
     public static final String KAFKA_GROUP_ID = "kafka.group.id";
     public static final String METRICS_TOPIC = "metrics.topic";
     public static final String CONSUMER_FROM_TIME = "consumer.from.time";
     public static final String STREAM_PARALLELISM = "stream.parallelism";
     public static final String STREAM_SINK_PARALLELISM = "stream.sink.parallelism";
+    public static final String STREAM_DEFAULT_PARALLELISM = "stream.default.parallelism";
     public static final String STREAM_CHECKPOINT_ENABLE = "stream.checkpoint.enable";
     public static final String STREAM_CHECKPOINT_INTERVAL = "stream.checkpoint.interval";
     public static final String PROPERTIES_FILE_NAME = "/application.properties";
