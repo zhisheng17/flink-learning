d5aa4ce1dea876603943dba8b61756ea58534396
zhisheng <zhisheng.tzs@alibaba-inc.com>
2019-03-04 19:45:05 +0800

    update flink connector kafka

--- flink-learning-connectors/flink-learning-connectors-kafka/src/main/java/com/zhisheng/connectors/kafka/Main.java
+++ flink-learning-connectors/flink-learning-connectors-kafka/src/main/java/com/zhisheng/connectors/kafka/Main.java
@@ -11,7 +11,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         final ParameterTool parameterTool = ExecutionEnvUtil.createParameterTool(args);
         StreamExecutionEnvironment env = ExecutionEnvUtil.prepare(parameterTool);
         DataStreamSource<Metrics> data = KafkaConfigUtil.buildSource(env);
@@ -20,7 +20,7 @@ public class Main {
                 parameterTool.get("kafka.sink.brokers"),
                 parameterTool.get("kafka.sink.topic"),
                 new MetricSchema()
-                )).name("flink-connectors-kafka")
+        )).name("flink-connectors-kafka")
                 .setParallelism(parameterTool.getInt("stream.sink.parallelism"));
 
         env.execute("flink learning connectors kafka");
