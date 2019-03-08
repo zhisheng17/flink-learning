5b20b46e6c37cf0a5e7ac60711b6386a51b39df3
zhisheng <zhisheng.tzs@alibaba-inc.com>
2019-03-08 14:05:55 +0800

    init flink learning connectors hdfs

---
+++ flink-learning-connectors/flink-learning-connectors-hdfs/src/main/java/com/zhisheng/connectors/hdfs/Main.java
@@ -0,0 +1,26 @@
+package com.zhisheng.connectors.hdfs;
+
+
+import com.zhisheng.common.model.Metrics;
+import com.zhisheng.common.utils.ExecutionEnvUtil;
+import com.zhisheng.common.utils.KafkaConfigUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * blog：http://www.54tianzhisheng.cn/
+ * 微信公众号：zhisheng
+ */
+@Slf4j
+public class Main {
+    public static void main(String[] args) throws Exception {
+        final ParameterTool parameterTool = ExecutionEnvUtil.createParameterTool(args);
+        StreamExecutionEnvironment env = ExecutionEnvUtil.prepare(parameterTool);
+        DataStreamSource<Metrics> data = KafkaConfigUtil.buildSource(env);
+
+
+        env.execute("flink learning connectors es6");
+    }
+}
