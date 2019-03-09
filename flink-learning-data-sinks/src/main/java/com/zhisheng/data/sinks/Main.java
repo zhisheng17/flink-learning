202849a3e292c41eb0226e147f8bb0461501ccff
zhisheng17 <1041218129@qq.com>
2019-03-09 17:51:19 +0800

    fix data sink

--- flink-learning-data-sinks/src/main/java/com/zhisheng/data/sinks/Main.java
+++ flink-learning-data-sinks/src/main/java/com/zhisheng/data/sinks/Main.java
@@ -1,15 +1,20 @@
 package com.zhisheng.data.sinks;
 
 
+import com.zhisheng.common.utils.ExecutionEnvUtil;
 import com.zhisheng.common.utils.GsonUtil;
+import com.zhisheng.common.utils.KafkaConfigUtil;
 import com.zhisheng.data.sinks.model.Student;
 import com.zhisheng.data.sinks.sinks.SinkToMySQL;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 
 import java.util.Properties;
+
+import static com.zhisheng.common.constant.PropertiesConstants.METRICS_TOPIC;
 
 /**
  * blog：http://www.54tianzhisheng.cn/
@@ -18,17 +23,11 @@ import java.util.Properties;
 public class Main {
     public static void main(String[] args) throws Exception{
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("zookeeper.connect", "localhost:2181");
-        props.put("group.id", "metric-group");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("auto.offset.reset", "latest");
+        ParameterTool parameterTool = ExecutionEnvUtil.PARAMETER_TOOL;
+        Properties props = KafkaConfigUtil.buildKafkaProps(parameterTool);
 
         SingleOutputStreamOperator<Student> student = env.addSource(new FlinkKafkaConsumer011<>(
-                "student",   //这个 kafka topic 需要和上面的工具类的 topic 一致
+                parameterTool.get(METRICS_TOPIC),   //这个 kafka topic 需要和上面的工具类的 topic 一致
                 new SimpleStringSchema(),
                 props)).setParallelism(1)
                 .map(string -> GsonUtil.fromJson(string, Student.class)); //博客里面用的是 fastjson，这里用的是gson解析，解析字符串成 student 对象
