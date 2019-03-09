26d9a8f342da7c2bdc4b4ea3d42f1d3b7db7dff5
zhisheng17 <1041218129@qq.com>
2019-03-09 17:53:04 +0800

    fix flink connector hbase

--- flink-learning-connectors/flink-learning-connectors-hbase/src/main/java/com/zhisheng/connectors/hbase/Main.java
+++ flink-learning-connectors/flink-learning-connectors-hbase/src/main/java/com/zhisheng/connectors/hbase/Main.java
@@ -1,13 +1,29 @@
 package com.zhisheng.connectors.hbase;
 
 
-import com.zhisheng.common.model.Metrics;
 import com.zhisheng.common.utils.ExecutionEnvUtil;
 import com.zhisheng.common.utils.KafkaConfigUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.net.ntp.TimeStamp;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+
+import static com.zhisheng.connectors.hbase.constant.HBaseConstant.*;
 
 /**
  * blog：http://www.54tianzhisheng.cn/
@@ -15,12 +31,50 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
  */
 @Slf4j
 public class Main {
+    private static TableName tableName = TableName.valueOf("zhisheng");
+    private static final String CF = "ke";
+
     public static void main(String[] args) throws Exception {
         final ParameterTool parameterTool = ExecutionEnvUtil.createParameterTool(args);
         StreamExecutionEnvironment env = ExecutionEnvUtil.prepare(parameterTool);
-        DataStreamSource<Metrics> data = KafkaConfigUtil.buildSource(env);
+        Properties props = KafkaConfigUtil.buildKafkaProps(parameterTool);
 
+        DataStreamSource<String> data = env.addSource(new FlinkKafkaConsumer011<>(
+                "zhisheng",   //这个 kafka topic 需要和上面的工具类的 topic 一致
+                new SimpleStringSchema(),
+                props));
+
+        data.map(new MapFunction<String, Object>() {
+            @Override
+            public Object map(String string) throws Exception {
+                writeEventToHbase(string, parameterTool);
+                return string;
+            }
+        }).print();
 
         env.execute("flink learning connectors hbase");
+    }
+
+    private static void writeEventToHbase(String string, ParameterTool parameterTool) throws IOException {
+        Configuration configuration = HBaseConfiguration.create();
+        configuration.set(HBASE_ZOOKEEPER_QUORUM, parameterTool.get(HBASE_ZOOKEEPER_QUORUM));
+        configuration.set(HBASE_ZOOKEEPER_PROPERTY_CLIENTPORT, parameterTool.get(HBASE_ZOOKEEPER_PROPERTY_CLIENTPORT));
+        configuration.set(HBASE_RPC_TIMEOUT, parameterTool.get(HBASE_RPC_TIMEOUT));
+        configuration.set(HBASE_CLIENT_OPERATION_TIMEOUT, parameterTool.get(HBASE_CLIENT_OPERATION_TIMEOUT));
+        configuration.set(HBASE_CLIENT_SCANNER_TIMEOUT_PERIOD, parameterTool.get(HBASE_CLIENT_SCANNER_TIMEOUT_PERIOD));
+
+        Connection connect = ConnectionFactory.createConnection(configuration);
+        Admin admin = connect.getAdmin();
+        if (!admin.tableExists(tableName)) {
+            admin.createTable(new HTableDescriptor(tableName).addFamily(new HColumnDescriptor(CF)));
+        }
+        Table table = connect.getTable(tableName);
+        TimeStamp ts = new TimeStamp(new Date());
+        Date date = ts.getDate();
+        Put put = new Put(Bytes.toBytes(date.getTime()));
+        put.addColumn(Bytes.toBytes(CF), Bytes.toBytes("test"), Bytes.toBytes(string));
+        table.put(put);
+        table.close();
+        connect.close();
     }
 }
---
+++ flink-learning-connectors/flink-learning-connectors-hbase/src/main/java/com/zhisheng/connectors/hbase/constant/HBaseConstant.java
@@ -0,0 +1,14 @@
+package com.zhisheng.connectors.hbase.constant;
+
+
+public class HBaseConstant {
+
+    public static final String HBASE_ZOOKEEPER_QUORUM = "hbase.zookeeper.quorum";
+    public static final String HBASE_CLIENT_RETRIES_NUMBER = "hbase.client.retries.number";
+    public static final String HBASE_MASTER_INFO_PORT = "hbase.master.info.port";
+    public static final String HBASE_ZOOKEEPER_PROPERTY_CLIENTPORT = "hbase.zookeeper.property.clientPort";
+    public static final String HBASE_RPC_TIMEOUT = "hbase.rpc.timeout";
+    public static final String HBASE_CLIENT_OPERATION_TIMEOUT = "hbase.client.operation.timeout";
+    public static final String HBASE_CLIENT_SCANNER_TIMEOUT_PERIOD = "hbase.client.scanner.timeout.period";
+
+}
