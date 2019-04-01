024425b8573854f595e3b34ffaacbcfaa74170b8
zhisheng17 <1041218129@qq.com>
2019-04-01 11:06:00 +0800

    update the wordcount program

--- flink-learning-examples/src/main/java/com/zhisheng/examples/wordcount/Main.java
+++ flink-learning-examples/src/main/java/com/zhisheng/examples/wordcount/Main.java
@@ -4,7 +4,6 @@ package com.zhisheng.examples.wordcount;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.util.Collector;
@@ -16,19 +15,20 @@ public class Main {
         final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
         env.getConfig().setGlobalJobParameters(params);
 
-        DataSource<String> data = env.fromElements(WORDS);
-        data.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
-            @Override
-            public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
-                String[] splits = value.toLowerCase().split("\\W+");
+        env.fromElements(WORDS)
+                .flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+                    @Override
+                    public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+                        String[] splits = value.toLowerCase().split("\\W+");
 
-                for (String split : splits) {
-                    if (split.length() > 0) {
-                        out.collect(new Tuple2<>(split, 1));
+                        for (String split : splits) {
+                            if (split.length() > 0) {
+                                out.collect(new Tuple2<>(split, 1));
+                            }
+                        }
                     }
-                }
-            }
-        }).groupBy(0)
+                })
+                .groupBy(0)
                 .reduce(new ReduceFunction<Tuple2<String, Integer>>() {
                     @Override
                     public Tuple2<String, Integer> reduce(Tuple2<String, Integer> value1, Tuple2<String, Integer> value2) throws Exception {
