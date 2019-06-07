04f20af762d003f631d3cca4ea27b1cd5464c31e
zhisheng17 <1041218129@qq.com>
2019-06-07 16:46:22 +0800

    :bento: Add flink Accumulator

---
+++ flink-learning-examples/src/main/java/com/zhisheng/examples/batch/accumulator/Main.java
@@ -0,0 +1,80 @@
+package com.zhisheng.examples.batch.accumulator;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.util.Collector;
+
+/**
+ * Desc: 累加器
+ * Created by zhisheng on 2019-06-07
+ * blog：http://www.54tianzhisheng.cn/
+ * 微信公众号：zhisheng
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        final ParameterTool params = ParameterTool.fromArgs(args);
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        env.getConfig().setGlobalJobParameters(params);
+
+        DataSource<String> dataSource = env.fromElements(WORDS);
+
+        dataSource.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String line, Collector<Tuple2<String, Integer>> out) throws Exception {
+                String[] words = line.split("\\W+");
+                for (String word : words) {
+                    out.collect(new Tuple2<>(word, 1));
+                }
+            }
+        })
+                .groupBy(0)
+                .sum(1)
+                .print();
+
+        long count = dataSource.count();
+        System.out.println(count);
+    }
+
+
+    private static final String[] WORDS = new String[]{
+            "To be, or not to be,--that is the question:--",
+            "Whether 'tis nobler in the mind to suffer",
+            "The slings and arrows of outrageous fortune",
+            "Or to take arms against a sea of troubles,",
+            "And by opposing end them?--To die,--to sleep,--",
+            "No more; and by a sleep to say we end",
+            "The heartache, and the thousand natural shocks",
+            "That flesh is heir to,--'tis a consummation",
+            "Devoutly to be wish'd. To die,--to sleep;--",
+            "To sleep! perchance to dream:--ay, there's the rub;",
+            "For in that sleep of death what dreams may come,",
+            "When we have shuffled off this mortal coil,",
+            "Must give us pause: there's the respect",
+            "That makes calamity of so long life;",
+            "For who would bear the whips and scorns of time,",
+            "The oppressor's wrong, the proud man's contumely,",
+            "The pangs of despis'd love, the law's delay,",
+            "The insolence of office, and the spurns",
+            "That patient merit of the unworthy takes,",
+            "When he himself might his quietus make",
+            "With a bare bodkin? who would these fardels bear,",
+            "To grunt and sweat under a weary life,",
+            "But that the dread of something after death,--",
+            "The undiscover'd country, from whose bourn",
+            "No traveller returns,--puzzles the will,",
+            "And makes us rather bear those ills we have",
+            "Than fly to others that we know not of?",
+            "Thus conscience does make cowards of us all;",
+            "And thus the native hue of resolution",
+            "Is sicklied o'er with the pale cast of thought;",
+            "And enterprises of great pith and moment,",
+            "With this regard, their currents turn awry,",
+            "And lose the name of action.--Soft you now!",
+            "The fair Ophelia!--Nymph, in thy orisons",
+            "Be all my sins remember'd."
+    };
+}
