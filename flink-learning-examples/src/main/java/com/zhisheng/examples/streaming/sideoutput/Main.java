42ca3404276688b5dfc11d39b0dc404f6b40d073
zhisheng17 <1041218129@qq.com>
2019-06-14 14:35:42 +0800

    :bento: Add flink side output example

--- flink-learning-examples/src/main/java/com/zhisheng/examples/streaming/sideoutput/Main.java
+++ flink-learning-examples/src/main/java/com/zhisheng/examples/streaming/sideoutput/Main.java
@@ -4,10 +4,9 @@ import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
-import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
 
@@ -19,7 +18,9 @@ import org.apache.flink.util.OutputTag;
  */
 public class Main {
 
-    private static final OutputTag<String> rejectedWordsTag = new OutputTag<String>("rejected") {
+    private static final OutputTag<String> overFiveTag = new OutputTag<String>("overFive") {
+    };
+    private static final OutputTag<String> equalFiveTag = new OutputTag<String>("equalFive") {
     };
 
     public static void main(String[] args) throws Exception {
@@ -28,7 +29,7 @@ public class Main {
         env.setStreamTimeCharacteristic(TimeCharacteristic.IngestionTime);
         env.getConfig().setGlobalJobParameters(params);
 
-        env.fromElements(WORDS)
+        SingleOutputStreamOperator<Tuple2<String, Integer>> tokenizer = env.fromElements(WORDS)
                 .keyBy(new KeySelector<String, Integer>() {
                     private static final long serialVersionUID = 1L;
 
@@ -37,11 +38,17 @@ public class Main {
                         return 0;
                     }
                 })
-                .process(new Tokenizer())
-                .keyBy(0)
-                .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+                .process(new Tokenizer());
+
+//        tokenizer.getSideOutput(overFiveTag).print();   //将字符串长度大于 5 的打印出来
+
+//        tokenizer.getSideOutput(equalFiveTag).print();  //将字符串长度等于 5 的打印出来
+
+//        tokenizer.print();  //这个打印出来的是字符串长度小于 5 的
+
+        tokenizer.keyBy(0)
                 .sum(1)
-                .print();
+                .print();   //做 word count 后打印出来
 
         env.execute("Streaming WordCount SideOutput");
     }
@@ -55,8 +62,10 @@ public class Main {
 
             for (String token : tokens) {
                 if (token.length() > 5) {
-                    ctx.output(rejectedWordsTag, token); //将字符串大于 5 的 word 放到 rejectedWordsTag 中去
-                } else if (token.length() > 0) {
+                    ctx.output(overFiveTag, token); //将字符串长度大于 5 的 word 放到 overFiveTag 中去
+                } else if (token.length() == 5) {
+                    ctx.output(equalFiveTag, token); //将字符串长度等于 5 的 word 放到 equalFiveTag 中去
+                } else if (token.length() < 5) {
                     out.collect(new Tuple2<>(token, 1));
                 }
             }
