a321c0968d6b13b0a9aa6ca9c545c5740aea5566
zhisheng17 <1041218129@qq.com>
2019-08-10 21:50:41 +0800

    :bento: Add flink broadcast example

---
+++ flink-learning-examples/src/main/java/com/zhisheng/examples/streaming/broadcast/GetAlarmNotifyData.java
@@ -0,0 +1,80 @@
+package com.zhisheng.examples.streaming.broadcast;
+
+
+import com.zhisheng.examples.util.MySQLUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.zhisheng.common.constant.PropertiesConstants.*;
+
+@Slf4j
+public class GetAlarmNotifyData extends RichSourceFunction<Map<String, String>> {
+
+    private Connection connection = null;
+    private PreparedStatement ps = null;
+    private volatile boolean isRunning = true;
+    private ParameterTool parameterTool;
+
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+        parameterTool = (ParameterTool) (getRuntimeContext().getExecutionConfig().getGlobalJobParameters());
+
+        String database = parameterTool.get(MYSQL_DATABASE);
+        String host = parameterTool.get(MYSQL_HOST);
+        String password = parameterTool.get(MYSQL_PASSWORD);
+        String port = parameterTool.get(MYSQL_PORT);
+        String username = parameterTool.get(MYSQL_USERNAME);
+
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://" + host + ":" + port + "/" + database + "?useUnicode=true&characterEncoding=UTF-8";
+        connection = MySQLUtil.getConnection(driver, url, username, password);
+
+        String sql = "select * from alarm_notify";
+        ps = connection.prepareStatement(sql);
+    }
+
+    @Override
+    public void run(SourceContext<Map<String, String>> ctx) throws Exception {
+        Map<String, String> map = new HashMap<>();
+        while (isRunning) {
+            ResultSet resultSet = ps.executeQuery();
+            while (resultSet.next()) {
+                if (0 == resultSet.getInt("status")) {
+                    map.put(resultSet.getString("type") + resultSet.getString("type_id"), resultSet.getString("target_id"));
+                }
+            }
+            log.info("=======select alarm notify from mysql, size = {}, map = {}", map.size(), map);
+
+            ctx.collect(map);
+            map.clear();
+            Thread.sleep(2000 * 60);
+        }
+
+    }
+
+    @Override
+    public void cancel() {
+        try {
+            super.close();
+            if (connection != null) {
+                connection.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
+        } catch (Exception e) {
+            log.error("runException:{}", e);
+        }
+        isRunning = false;
+    }
+}
---
+++ flink-learning-examples/src/main/java/com/zhisheng/examples/streaming/broadcast/Main.java
@@ -0,0 +1,82 @@
+package com.zhisheng.examples.streaming.broadcast;
+
+import com.zhisheng.common.model.MetricEvent;
+import com.zhisheng.common.utils.ExecutionEnvUtil;
+import com.zhisheng.common.utils.KafkaConfigUtil;
+import org.apache.flink.api.common.state.BroadcastState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.api.common.state.ReadOnlyBroadcastState;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.BroadcastStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
+import org.apache.flink.util.Collector;
+
+import java.util.Map;
+
+/**
+ * Desc: 广播变量，定时从数据库读取告警规则数据
+ * Created by zhisheng on 2019-05-30
+ * blog：http://www.54tianzhisheng.cn/
+ * 微信公众号：zhisheng
+ */
+public class Main {
+
+    final static MapStateDescriptor<String, String> ALARM_RULES = new MapStateDescriptor<>(
+            "alarm_rules",
+            BasicTypeInfo.STRING_TYPE_INFO,
+            BasicTypeInfo.STRING_TYPE_INFO);
+
+    public static void main(String[] args) throws Exception {
+
+        final ParameterTool parameterTool = ExecutionEnvUtil.createParameterTool(args);
+        StreamExecutionEnvironment env = ExecutionEnvUtil.prepare(parameterTool);
+
+        BroadcastStream<Map<String, String>> broadcast = env.addSource(new GetAlarmNotifyData()).setParallelism(1) //数据流定时从数据库中查出来数据
+                .broadcast(ALARM_RULES);    //广播出去
+
+
+        DataStreamSource<MetricEvent> metricEventDataStream = KafkaConfigUtil.buildSource(env);
+        SingleOutputStreamOperator<MetricEvent> alert = metricEventDataStream.connect(broadcast)
+                .process(new BroadcastProcessFunction<MetricEvent, Map<String, String>, MetricEvent>() {
+
+                    private MapStateDescriptor<String, String> alarmRulesMapStateDescriptor;
+
+                    @Override
+                    public void processElement(MetricEvent value, ReadOnlyContext ctx, Collector<MetricEvent> out) throws Exception {
+                        ReadOnlyBroadcastState<String, String> broadcastState = ctx.getBroadcastState(alarmRulesMapStateDescriptor);
+                        Map<String, String> tags = value.getTags();
+                        if (!tags.containsKey("type") && !tags.containsKey("type_id")) {
+                            return;
+                        }
+                        String targetId = broadcastState.get(tags.get("type") + tags.containsKey("type_id"));
+                        if (targetId != null) {
+                            value.getTags().put("target_id", targetId); //将通知方式的 hook 放在 tag 里面，在下游要告警的时候通过该字段获取到对应的 hook 地址
+                            out.collect(value);
+                        }
+                    }
+
+                    @Override
+                    public void processBroadcastElement(Map<String, String> value, Context ctx, Collector<MetricEvent> out) throws Exception {
+                        if (value != null) {
+                            BroadcastState<String, String> broadcastState = ctx.getBroadcastState(alarmRulesMapStateDescriptor);
+                            for (Map.Entry<String, String> entry : value.entrySet()) {
+                                broadcastState.put(entry.getKey(), entry.getValue());
+                            }
+                        }
+                    }
+                });
+
+
+        //其他的业务逻辑
+        //alert.
+
+        //然后在下游的算子中有使用到 alarmNotifyMap 中的配置信息
+
+
+        env.execute("spot alerting-send");
+    }
+}
