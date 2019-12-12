322b4bde338885a8dc1a631de3f44c6d186ab547
zhisheng <1041218129@qq.com>
2019-12-12 21:40:06 +0800

    add window function

---
+++ flink-learning-window/src/main/java/com/zhisheng/window/Main3.java
@@ -0,0 +1,35 @@
+package com.zhisheng.window;
+
+import com.zhisheng.common.model.WordEvent;
+import com.zhisheng.function.CustomSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+/**
+ * Desc:
+ * Created by zhisheng on 2019-12-12 21:29
+ * blog：http://www.54tianzhisheng.cn/
+ * 微信公众号：zhisheng
+ */
+public class Main3 {
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.addSource(new CustomSource())
+                .keyBy(WordEvent::getWord)
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(10)))
+                .apply(new WindowFunction<WordEvent, WordEvent, String, TimeWindow>() {
+                    @Override
+                    public void apply(String s, TimeWindow window, Iterable<WordEvent> input, Collector<WordEvent> out) throws Exception {
+                        for (WordEvent word : input) {
+                            out.collect(word);
+                        }
+                    }
+                })
+                .print();
+        env.execute();
+    }
+}
