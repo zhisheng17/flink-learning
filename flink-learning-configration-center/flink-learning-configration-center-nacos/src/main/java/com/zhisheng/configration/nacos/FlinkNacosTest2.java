80a5cdae6c063900b25d1b1d1a6c3d385d8a3e07
zhisheng17 <1041218129@qq.com>
2020-02-29 16:18:08 +0800

    add flink nacos checkpoint test

--- flink-learning-configration-center/flink-learning-configration-center-nacos/src/main/java/com/zhisheng/configration/nacos/FlinkNacosTest2.java
+++ flink-learning-configration-center/flink-learning-configration-center-nacos/src/main/java/com/zhisheng/configration/nacos/FlinkNacosTest2.java
@@ -14,7 +14,12 @@ import java.util.Properties;
 import java.util.concurrent.Executor;
 
 /**
- * Desc: 测试 nacos 动态更改 Checkpoint 配置后，Flink 是否可以获取到更改后的值，并生效
+ * Desc: 测试 nacos 动态更改 Checkpoint 配置后，Flink 是否可以获取到更改后的值，并生效?
+ *
+ * 结论是：不生效，因为 Flink 是 Lazy Evaluation（延迟执行），当程序的 main 方法执行时，数据源加载数据和数据转换等算子不会立马执行，
+ * 这些操作会被创建并添加到程序的执行计划中去，只有当执行环境 env 的 execute 方法被显示地触发执行时，整个程序才开始执行实际的操作，所以
+ * 在一开始初始化后等程序执行 execute 方法后再修改 env 的配置其实就不起作用了。
+ *
  * Created by zhisheng on 2020/2/29 下午3:27
  * blog：http://www.54tianzhisheng.cn/
  * 微信公众号：zhisheng
@@ -43,6 +48,8 @@ public class FlinkNacosTest2 {
                 System.out.println("===============");
                 content[0] = configInfo;
                 env.getCheckpointConfig().setCheckpointInterval(Long.valueOf(content[0]));
+                System.out.println("----------");
+                System.out.println(env.getCheckpointConfig().getCheckpointInterval());
             }
         });
         System.out.println(content[0]);
