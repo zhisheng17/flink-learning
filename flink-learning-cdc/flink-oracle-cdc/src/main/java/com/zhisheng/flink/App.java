8fa4722f85b9c3cd69c2ee5e69d0a8262101a5bc
zhisheng <zhisheng2018@gmail.com>
2022-12-21 16:08:58 +0800

    init the flink oracle cdc project

---
+++ flink-learning-cdc/flink-oracle-cdc/src/main/java/com/zhisheng/flink/App.java
@@ -0,0 +1,13 @@
+package com.zhisheng.flink;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
